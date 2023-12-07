86905fc0bd18648f1ba05333ef06d16d22deacc3
lynzr-1 <lriches1@my.centennialcollege.ca>
2023-12-06 20:59:28 -0500

    Adjust assets-router.js

--- server/assets-router.js
+++ server/assets-router.js
@@ -5,10 +5,12 @@ const videoRegex = /\/.+\.(mp4|ogv)$/
 
 router.get(imageRegex, (req, res) => {
   const filePath = req.path;
-  res.redirect(303, `http://localhost:3000/src${filePath}`);
+  const baseUrl = `${req.protocol}://${req.get('host')}`;
+  res.redirect(303, `${baseUrl}/src${filePath}`);
 });
 router.get(videoRegex, (req, res) => {
   const filePath = req.path;
-  res.redirect(303, `http://localhost:3000/src${filePath}`);
+  const baseUrl = `${req.protocol}://${req.get('host')}`;
+  res.redirect(303, `${baseUrl}/src${filePath}`);
 });
 module.exports = router;
